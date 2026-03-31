d93495dfc65050bb8db405fd40cfbc07a8a90c04
Heena Patel <277heena@gmail.com>
2026-03-30 18:41:57 -0700

    Added gallery info box with click event

--- script.js
+++ script.js
@@ -1,88 +1,62 @@
-let images = [
-    "images/winterland1.jpg",
-    "images/winterland2.jpg",
-    "images/winterland3.jpg",
-    "images/winterland4.jpg",
-    "images/winterland5.jpg",
-    "images/winterland6.jpg",
-    "images/central_park.jpg",
-    "images/east-village.jpg",
-    "images/winterland1.jpg",
-    "images/winterland2.jpg"
+var captions = [
+  "Snow Land",
+  "Wintery Road",
+  "Snowy Road",
+  "Winter in the Country"
 ];
 
-let captions = [
-    "Winter Scene 1",
-    "Winter Scene 2",
-    "Winter Scene 3",
-    "Winter Scene 4",
-    "Winter Scene 5",
-    "Winter Scene 6",
-    "Central Park",
-    "East Village",
-    "Snow View 1",
-    "Snow View 2"
+var descriptions = [
+  "After an overnight snowstorm, the land looks peaceful and bright. Winter may seem quiet, but it creates a beautiful and magical scene.",
+  "The most beautiful phenomenon of winter is snow. Every snowflake is different, which makes each snowy road feel special.",
+  "Driving on a snowy road can feel calm and silent. The white landscape and soft light make winter roads look stunning.",
+  "Winter in the country has a special charm. The snowy fields and trees create a quiet and relaxing atmosphere."
 ];
 
-let descriptions = [
-    "A peaceful snowy landscape",
-    "Cold winter morning view",
-    "Snow covered trees",
-    "Beautiful winter path",
-    "Chilly nature scene",
-    "Frozen forest view",
-    "A winter view of Central Park",
-    "A calm city winter scene",
-    "Another quiet snow scene",
-    "A soft and peaceful winter day"
+var fileNames = [
+  "winterland1",
+  "winterland2",
+  "winterland3",
+  "winterland4"
 ];
 
-let gallery = document.getElementById("gallery");
+var imageList = [];
+var openList = "<li class='gallery-item'>";
+var closeList = "</li>";
+var closeText = "Click This To Close";
 
-if (gallery) {
-    for (let i = 0; i < images.length; i++) {
-        gallery.innerHTML +=
-            "<li id='photo" + (i + 1) + "'>" +
-            "<img src='" + images[i] + "' alt='" + captions[i] + "'>" +
-            "<p class='caption'>" + captions[i] + "</p>" +
-            "<p class='description'>" + descriptions[i] + "</p>" +
-            "</li>";
-    }
+for (var i = 0; i < fileNames.length; i++) {
+  imageList.push(
+    openList +
+    "<img src='images/" + fileNames[i] + ".jpg' alt='" + captions[i] + "'>" +
+    "<div class='caption'>" + captions[i] + "</div>" +
+    "<div class='description-bar' data-index='" + i + "'>" + descriptions[i] + "</div>" +
+    closeList
+  );
 }
 
-function currentTime() {
-    var d = new Date();
-    var hr = d.getHours();
-    var min = d.getMinutes();
-    var sec = d.getSeconds();
-    var ampm;
+document.getElementById("gallery").innerHTML = imageList.join("");
 
-    if (sec < 10) {
-        sec = "0" + sec;
-    }
+var infoBox = document.getElementById("infobox");
+var infoHeading = document.getElementById("infoheading");
+var infoText = document.getElementById("infotext");
+var closeBox = document.getElementById("closebox");
 
-    if (min < 10) {
-        min = "0" + min;
-    }
+closeBox.innerHTML = closeText;
 
-    if (hr == 0) {
-        hr = 12;
-        ampm = "AM";
-    } else if (hr < 12) {
-        ampm = "AM";
-    } else if (hr == 12) {
-        ampm = "PM";
-    } else {
-        hr = hr - 12;
-        ampm = "PM";
-    }
+var descriptionBars = document.querySelectorAll(".description-bar");
 
-    var time = hr + ":" + min + ":" + sec + " " + ampm;
+for (var j = 0; j < descriptionBars.length; j++) {
+  descriptionBars[j].addEventListener("click", function () {
+    var index = this.getAttribute("data-index");
 
-    document.getElementById("clock").innerHTML = time;
+    infoHeading.innerHTML = captions[index];
+    infoText.innerHTML = descriptions[index];
+    closeBox.innerHTML = closeText;
+    infoBox.style.visibility = "visible";
+  });
 }
 
-if (document.getElementById("clock")) {
-    currentTime();
-    setInterval(currentTime, 1000);
-}
+closeBox.addEventListener("click", function (event) {
+  event.preventDefault();
+  infoBox.style.visibility = "hidden";
+});
